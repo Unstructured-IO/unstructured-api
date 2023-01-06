c94d8067b244c3523baa4d7d3c51f1296a45b12d
Crag Wolfe <crag@unstructured.io>
2023-01-06 07:18:31 +0000

    chore: Use standard pipeline-repo docker patterns

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-DOCKER_BUILDKIT=1 docker buildx build --platform=linux/amd64 -f docker/Dockerfile"${BUILD_TYPE}" \
+DOCKER_BUILDKIT=1 docker buildx build --platform=linux/amd64 -f Dockerfile \
   --build-arg PIP_VERSION="$PIP_VERSION" \
   --build-arg PIPELINE_FAMILY="$PIPELINE_FAMILY" \
   --progress plain \
-  -t pipeline-family-"$PIPELINE_FAMILY"-dev"${BUILD_TYPE}":latest .
+  -t pipeline-family-"$PIPELINE_FAMILY"-dev:latest .
