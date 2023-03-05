3356f3547f44efdb18d66d385b14d7192ff15bb7
Crag Wolfe <crag@unstructuredai.io>
2023-03-04 17:15:15 -0800

    tests

--- test_general/api/test_app.py
+++ test_general/api/test_app.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+import time
+
+import pytest
+
 from fastapi.testclient import TestClient
 
 from unstructured_api_tools.pipelines.api_conventions import get_pipeline_path
-
 
 from prepline_general.api.app import app
 
@@ -9,37 +13,46 @@ MAIN_API_ROUTE = get_pipeline_path("general")
 
 
 def test_general_api_health_check():
+    # NOTE(robinson) - Reset the rate limit to avoid 429s in tests
+    app.state.limiter.reset()
     client = TestClient(app)
     response = client.get("/healthcheck")
 
     assert response.status_code == 200
 
 
-# layout-parser-paper.pdf
-# layout-parser-paper-fast.jpg
-# fake.doc
-# fake-power-point.ppt
-# family-day.eml
-# fake.docx
-# fake-text.txt
-# fake-power-point.pptx
-# fake-html.html
-# fake-excel.xlsx
-# fake-email.eml
-# fake-email-image-embedded.eml
-# fake-email-attachment.eml
-# announcement.eml
-# alert.eml
-
-
-def test_general_api_health_check():
-    client = TestClient(app)
-
+@pytest.mark.parametrize(
+    "example_filename",
+    [
+        "layout-parser-paper.pdf",
+        "layout-parser-paper-fast.jpg",
+        "fake.doc",
+        "fake-power-point.ppt",
+        "family-day.eml",
+        "fake.docx",
+        "fake-text.txt",
+        "fake-power-point.pptx",
+        "fake-html.html",
+        "fake-excel.xlsx",
+        "fake-email.eml",
+        "fake-email-image-embedded.eml",
+        "fake-email-attachment.eml",
+        "announcement.eml",
+        "alert.eml",
+    ],
+)
+def test_general_api(example_filename):
+    # NOTE(crag) - Reset below doesn't seem to be working, not sure why. But rate
+    # limiting will be removed soon anyway
+    time.sleep(1)
     # NOTE(robinson) - Reset the rate limit to avoid 429s in tests
     app.state.limiter.reset()
     client = TestClient(app)
+    test_file = Path("sample-docs") / example_filename
     response = client.post(
-        SECTION_ROUTE,
-        files=[("text_files", (filename, open(filename, "rb"), "text/plain"))],
-        data={"output_schema": "labelstudio", "section": [section]},
+        MAIN_API_ROUTE, files=[("files", (str(test_file), open(test_file, "rb"), "text/plain"))]
     )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert len("".join(elem["text"] for elem in response.json())) > 20
+    time.sleep(1)
