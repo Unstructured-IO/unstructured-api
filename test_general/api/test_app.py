82efb4d2012d77f06812c8e14c0c48799a1dbc38
Crag Wolfe <crag@unstructuredai.io>
2023-03-04 16:31:22 -0800

    pip-compile bump

--- test_general/api/test_app.py
+++ test_general/api/test_app.py
@@ -1,7 +1,12 @@
 from fastapi.testclient import TestClient
 
+from unstructured_api_tools.pipelines.api_conventions import get_pipeline_path
+
 
 from prepline_general.api.app import app
+
+MAIN_API_ROUTE = get_pipeline_path("general")
+
 
 
 def test_general_api_health_check():
@@ -9,3 +14,31 @@ def test_general_api_health_check():
     response = client.get("/healthcheck")
 
     assert response.status_code == 200
+
+#layout-parser-paper.pdf
+#layout-parser-paper-fast.jpg
+#fake.doc
+#fake-power-point.ppt
+#family-day.eml
+#fake.docx
+#fake-text.txt
+#fake-power-point.pptx
+#fake-html.html
+#fake-excel.xlsx
+#fake-email.eml
+#fake-email-image-embedded.eml
+#fake-email-attachment.eml
+#announcement.eml
+#alert.eml
+
+def test_general_api_health_check():
+    client = TestClient(app)
+
+    # NOTE(robinson) - Reset the rate limit to avoid 429s in tests
+    app.state.limiter.reset()
+    client = TestClient(app)
+    response = client.post(
+        SECTION_ROUTE,
+        files=[("text_files", (filename, open(filename, "rb"), "text/plain"))],
+        data={"output_schema": "labelstudio", "section": [section]},
+    )
