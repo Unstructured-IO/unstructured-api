30a52dce0adbb48842d4a81749ea14c4fceb469f
Austin Walker <austin@unstructured.io>
2023-03-15 18:31:11 -0400

    Add some files back to unit and smoke tests
    Sort the parameter lists

--- scripts/smoketest.py
+++ scripts/smoketest.py
@@ -14,21 +14,21 @@ def send_document(filename):
     "example_filename",
     [
         "alert.eml",
+        "announcement.eml",
+        "fake-email-attachment.eml",
+        "fake-email-image-embedded.eml",
+        "fake-email.eml",
+        "fake-html.html",
+        pytest.param("fake-power-point.ppt", marks=pytest.mark.xfail(reason="See CORE-796")),
+        "fake-text.txt",
+        "fake.doc",
+        "fake.docx",
+        "family-day.eml",
+        pytest.param("fake-excel.xlsx", marks=pytest.mark.xfail(reason="not supported yet")),
         # Note(austin) The two inference calls will hang on mac with unsupported hardware error
         # Need to handle this better
         "layout-parser-paper.pdf",
         "layout-parser-paper-fast.jpg",
-        pytest.param("fake.doc", marks=pytest.mark.xfail(reason="needs investigation")),
-        pytest.param("fake-power-point.ppt", marks=pytest.mark.xfail(reason="needs investigation")),
-        "family-day.eml",
-        "fake.docx",
-        pytest.param("fake-text.txt", marks=pytest.mark.xfail(reason="needs investigation")),
-        "fake-html.html",
-        pytest.param("fake-excel.xlsx", marks=pytest.mark.xfail(reason="needs investigation")),
-        "fake-email.eml",
-        "fake-email-image-embedded.eml",
-        "fake-email-attachment.eml",
-        "announcement.eml",
     ]
 )
 
