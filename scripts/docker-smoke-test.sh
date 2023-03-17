ca8f1ab3c84e28462775ad91aad99ac50cf7cb38
Austin Walker <austin@unstructured.io>
2023-03-17 10:27:56 -0400

    chore: add initial smoke test running against containerized api (#49)
    * Add tesseract to dockerfile, fix entrypoint
    
    * chore: add initial smoke test to be run in github actions
    
    * Clean up the container on test exit, preserve exit code
    
    * Add the venv back to make sure we have pytest
    
    * Remove unneeded entrypoint arg in make docker-start-api
    
    * Add some files back to unit and smoke tests
    
    Sort the parameter lists
    
    * Make sure we run setup before docker test
    
    * Change the CI python cache to use a valid requirements file
    
    * Add libreoffice as a ci dependency
    
    * Don't run docker test if linter fails
    
    * Update smoke test script to do readiness check first
    
    * Appease the shellcheck gods
    
    * Quicker readiness checks
    
    * Remove unneeded layers of changing the default port

---
+++ scripts/docker-smoke-test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# docker-smoke-test.sh
+# Start the containerized api and run some end-to-end tests against it
+# There will be some overlap with just running a TestClient in the unit tests
+# Is there a good way to reuse code here?
+# Also note this can evolve into a generalized pipeline smoke test
+
+# shellcheck disable=SC2317  # Shellcheck complains that trap functions are unreachable...
+
+container_name=pipeline-general
+image_name=pipeline-family-general-dev:latest
+
+start_container() {
+    echo Starting container "$container_name"
+    docker run -p 8000:8000 -d --rm --name "$container_name" "$image_name" --port 8000 --host 0.0.0.0
+}
+
+await_server_ready() {
+    url=localhost:8000/healthcheck
+
+    for _ in {1..15}; do
+        echo Waiting for response from "$url"
+        if curl $url 2> /dev/null; then
+            echo
+            return
+        fi
+
+        sleep 1
+    done
+
+    echo Server did not respond!
+    exit 1
+}
+
+stop_container() {
+    echo Stopping container "$container_name"
+    docker stop "$container_name"
+}
+
+start_container
+
+# Regardless of test result, stop the container
+trap stop_container EXIT
+
+await_server_ready
+
+echo Running tests
+PYTHONPATH=. pytest scripts/smoketest.py
+
+result=$?
+exit $result
