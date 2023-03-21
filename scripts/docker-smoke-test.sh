94da29773124998256a9b659210ccc3075e73389
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-03-21 08:17:27 -0700

    fix image name

--- scripts/docker-smoke-test.sh
+++ scripts/docker-smoke-test.sh
@@ -10,6 +10,7 @@
 
 CONTAINER_NAME=pipeline-general
 IMAGE_NAME="${IMAGE_NAME:-pipeline-family-general:latest}"
+echo Image name: "$IMAGE_NAME"
 
 start_container() {
     echo Starting container "$CONTAINER_NAME"
