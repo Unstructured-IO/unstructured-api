fe2b16ccdd0ec8a8238db34a92feb1022d79610a
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-04-04 22:33:15 -0700

    get corresponding arch tag for platform

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 DOCKER_BUILD_PLATFORM="${DOCKER_BUILD_PLATFORM:-linux/amd64}"
-DOCKER_BUILD_REPOSITORY="${DOCKER_BUILD_REPOSITORY:-quay.io/unstructured-io/unstructured-api}"
+if [[ $DOCKER_BUILD_PLATFORM == "linux/amd64" ]]; then
+    DOCKER_ARCH_TAG="amd"
+elif [[ $DOCKER_BUILD_PLATFORM == "linux/arm64" ]]; then
+    DOCKER_ARCH_TAG="arm"
+else
+    echo "Unsupported platform: $DOCKER_BUILD_PLATFORM"
+    exit 1
+fi
+DOCKER_BUILD_REPOSITORY="${DOCKER_BUILD_REPOSITORY:-quay.io/unstructured-io/build-unstructured-api}"
 PIPELINE_PACKAGE="${PIPELINE_PACKAGE:-general}"
 PIP_VERSION="${PIP_VERSION:-21.0.1}"
 
@@ -11,5 +19,5 @@ DOCKER_BUILDKIT=1 docker buildx build --load --platform="$DOCKER_BUILD_PLATFORM"
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --build-arg PIPELINE_PACKAGE="$PIPELINE_PACKAGE" \
   --progress plain \
-  --cache-from "$DOCKER_BUILD_REPOSITORY":latest \
+  --cache-from "$DOCKER_BUILD_REPOSITORY":$DOCKER_ARCH_TAG \
   -t pipeline-family-"$PIPELINE_FAMILY"-dev:latest .
