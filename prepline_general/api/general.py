f0673c2f80bbc86d518fb8af17fbe0bd8b916667
Crag Wolfe <crag@unstructuredai.io>
2023-04-05 21:25:53 -0700

    make generate-api

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -209,7 +209,7 @@ def ungz_file(file: UploadFile) -> UploadFile:
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.8/general")
+@router.post("/general/v0.0.9/general")
 def pipeline_1(
     request: Request,
     files: Union[List[UploadFile], None] = File(default=None),
