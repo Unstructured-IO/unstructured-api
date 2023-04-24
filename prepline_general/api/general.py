f652826fc8a9732493ea3fdef0d81bd44a9584fa
natygyoon <ygyoon@uwaterloo.ca>
2023-04-24 17:51:41 -0400

    nit

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -235,7 +235,7 @@ def ungz_file(file: UploadFile) -> UploadFile:
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.13/general")
+@router.post("/general/v0.0.14/general")
 def pipeline_1(
     request: Request,
     files: Union[List[UploadFile], None] = File(default=None),
