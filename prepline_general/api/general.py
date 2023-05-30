f59795785fec7838859ebffdf82f293c165fcc87
yuming <305248291@qq.com>
2023-05-30 10:23:09 -0400

    version sync

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -206,7 +206,7 @@ def ungz_file(file: UploadFile, gz_uncompressed_content_type=None) -> UploadFile
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.19/general")
+@router.post("/general/v0.0.20/general")
 def pipeline_1(
     request: Request,
     gz_uncompressed_content_type: Optional[str] = Form(default=None),
