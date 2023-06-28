b9b183ef7eddc933b62b4eb1a663484a6e51ddb9
Shreya Nidadavolu <shreyanid9@gmail.com>
2023-06-28 14:26:50 -0700

    update yaml version

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -361,7 +361,7 @@ def ungz_file(file: UploadFile, gz_uncompressed_content_type=None) -> UploadFile
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.29/general")
+@router.post("/general/v0.0.30/general")
 def pipeline_1(
     request: Request,
     gz_uncompressed_content_type: Optional[str] = Form(default=None),
