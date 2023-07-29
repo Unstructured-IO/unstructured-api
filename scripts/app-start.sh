af85399ef29bf707437fb59e3ddb0811850504cc
Crag Wolfe <crag@unstructuredai.io>
2023-07-29 15:02:26 -0700

    app-start.sh script

---
+++ scripts/app-start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+UNSTRUCTURED_DOWNLOAD_CHIPPER=${UNSTRUCTURED_DOWNLOAD_CHIPPER:-"false"}
+
+if [[ "$(echo "${UNSTRUCTURED_DOWNLOAD_CHIPPER}" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+  UNSTRUCTURED_HI_RES_SUPPORTED_MODEL=chipper python3.8 -c \
+    "from unstructured.ingest.doc_processor.generalized import initialize; initialize()"
+fi
+
+uvicorn prepline_general.api.app:app" \
+  "--log-config logger_config.yaml" \
+  "--host 0.0.0.0
