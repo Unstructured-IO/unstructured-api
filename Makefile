7d3139af46492c7ab876b2cc2b7827d2243f9267
Crag Wolfe <crag@unstructured.io>
2023-07-29 23:02:52 +0000

    fixes

--- Makefile
+++ Makefile
@@ -78,7 +78,7 @@ docker-build:
 
 .PHONY: docker-start-api
 docker-start-api:
-	docker run -p 8000:8000 --mount type=bind,source=$(realpath .),target=/home/notebook-user/local -t --rm pipeline-family-${PIPELINE_FAMILY}-dev:latest --log-config logger_config.yaml --host 0.0.0.0 --port 8000
+	docker run -p 8000:8000 --mount type=bind,source=$(realpath .),target=/home/notebook-user/local -t --rm pipeline-family-${PIPELINE_FAMILY}-dev:latest scripts/app-start.sh
 
 # Note(austin) we need to install the dev dependencies for this to work
 # Do we want to build separate dev images?
--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -3,10 +3,11 @@
 UNSTRUCTURED_DOWNLOAD_CHIPPER=${UNSTRUCTURED_DOWNLOAD_CHIPPER:-"false"}
 
 if [[ "$(echo "${UNSTRUCTURED_DOWNLOAD_CHIPPER}" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+  echo "warming chipper model"
   UNSTRUCTURED_HI_RES_SUPPORTED_MODEL=chipper python3.8 -c \
     "from unstructured.ingest.doc_processor.generalized import initialize; initialize()"
 fi
 
-uvicorn prepline_general.api.app:app" \
-  "--log-config logger_config.yaml" \
-  "--host 0.0.0.0
+uvicorn prepline_general.api.app:app \
+	--log-config logger_config.yaml \
+        --host 0.0.0.0
