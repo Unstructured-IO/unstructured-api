5db5df7b827d395d90a10adf45ab14e6ca60764c
Austin Walker <austin@unstructured.io>
2023-08-18 17:35:07 -0400

    chore(deps): bump unstructured

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -484,7 +484,7 @@ def ungz_file(file: UploadFile, gz_uncompressed_content_type=None) -> UploadFile
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.36/general")
+@router.post("/general/v0.0.37/general")
 def pipeline_1(
     request: Request,
     gz_uncompressed_content_type: Optional[str] = Form(default=None),
