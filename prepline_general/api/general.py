da01951e11c72cad905cd76a67e1a7de369cf62e
Austin Walker <austin@unstructured.io>
2023-10-25 11:26:30 -0400

    version sync

--- prepline_general/api/general.py
+++ prepline_general/api/general.py
@@ -594,7 +594,7 @@ def ungz_file(file: UploadFile, gz_uncompressed_content_type=None) -> UploadFile
 
 
 @router.post("/general/v0/general")
-@router.post("/general/v0.0.54/general")
+@router.post("/general/v0.0.55/general")
 def pipeline_1(
     request: Request,
     gz_uncompressed_content_type: Optional[str] = Form(default=None),
