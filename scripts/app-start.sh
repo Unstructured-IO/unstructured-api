999f663a7499217ee3bc7782bb7aee96f7f887e6
Austin Walker <austin@unstructured.io>
2023-10-26 14:43:53 -0400

    Remove the old way of loading chipper in app-start.sh

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-UNSTRUCTURED_DOWNLOAD_CHIPPER=${UNSTRUCTURED_DOWNLOAD_CHIPPER:-"false"}
-
-if [[ "$(echo "${UNSTRUCTURED_DOWNLOAD_CHIPPER}" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
-  echo "warming chipper model"
-  # NOTE(crag): in the cloud, this could add a minute to startup time
-  UNSTRUCTURED_HI_RES_SUPPORTED_MODEL=chipper python3.8 -c \
-    "from unstructured.ingest.doc_processor.generalized import initialize; initialize()"
-fi
-
 uvicorn prepline_general.api.app:app \
 	--log-config logger_config.yaml \
         --host 0.0.0.0
