6c01cb9d13d16a6f0756689f81232ee00ba3b55a
Yuming Long <63475068+yuming-long@users.noreply.github.com>
2023-11-20 15:22:21 -0500

    Chore: bump version to `0.0.59` and release (#312)
    bump unst version to `0.11.0` also revert the workaround in `0.0.58`

--- prepline_general/api/app.py
+++ prepline_general/api/app.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("unstructured_api")
 app = FastAPI(
     title="Unstructured Pipeline API",
     description="""""",
-    version="0.0.58",
+    version="0.0.59",
     docs_url="/general/docs",
     openapi_url="/general/openapi.json",
 )
