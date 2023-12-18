c89e51a2d79ed3756ab42fca755f438745895a89
coniferish <johnjennings702@gmail.com>
2023-12-18 13:10:01 -0600

    update versions

--- prepline_general/api/app.py
+++ prepline_general/api/app.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("unstructured_api")
 app = FastAPI(
     title="Unstructured Pipeline API",
     description="""""",
-    version="0.0.60",
+    version="0.0.61",
     docs_url="/general/docs",
     openapi_url="/general/openapi.json",
 )
