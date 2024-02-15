7370d6f7ae135099b921689fb9e20802b6636068
Christine Straub <christinemstraub@gmail.com>
2024-02-15 10:26:01 -0800

    feat: apply `SmartValueParser.value_or_first_element()` to all list type parameter

--- prepline_general/api/models/form_params.py
+++ prepline_general/api/models/form_params.py
@@ -52,6 +52,7 @@ class GeneralFormParams(BaseModel):
                 description="The languages present in the document, for use in partitioning and/or OCR",
                 example="[eng]",
             ),
+            BeforeValidator(SmartValueParser[List[str]]().value_or_first_element),
         ] = [],  # noqa
         ocr_languages: Annotated[
             List[str],
@@ -70,11 +71,7 @@ class GeneralFormParams(BaseModel):
                 example="['pdf', 'jpg', 'png']",
             ),
             BeforeValidator(SmartValueParser[List[str]]().value_or_first_element),
-        ] = [
-            "pdf",
-            "jpg",
-            "png",
-        ],  # noqa
+        ] = ["pdf", "jpg", "png"],  # noqa
         gz_uncompressed_content_type: Annotated[
             Optional[str],
             Form(
@@ -149,6 +146,7 @@ class GeneralFormParams(BaseModel):
                 description="The types of elements to extract, for use in extracting image blocks as base64 encoded data stored in metadata fields",
                 example="""["image", "table"]""",
             ),
+            BeforeValidator(SmartValueParser[List[str]]().value_or_first_element),
         ] = [],  # noqa
         # -- chunking options --
         chunking_strategy: Annotated[
