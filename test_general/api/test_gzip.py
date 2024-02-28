af5270a9ccf512e1cf21bbcec251ff21dfe93472
hubert.rutkowski <hubert.rutkowski@deepsense.ai>
2024-02-28 18:38:54 +0100

    test: extend test for two cases of output_format, and situation when ungzipped content_type is not set.

--- test_general/api/test_gzip.py
+++ test_general/api/test_gzip.py
@@ -1,7 +1,9 @@
+import io
 import tempfile
 from pathlib import Path
 
 import httpx
+import pandas as pd
 import pytest
 from fastapi.testclient import TestClient
 from deepdiff import DeepDiff
@@ -27,20 +29,45 @@ def call_api(client: TestClient, filename: str, content_type: str, options: dict
         data=options,
     )
     assert response.status_code == 200, response.text
-    assert len(response.json()) > 0
+    assert len(response.text) > 0
     return response
 
 
+def get_gzipped_response(client: TestClient, original_file: str, options: dict):
+    gz_file_extension =  f".{Path(original_file).suffix}.gz"
+    with tempfile.NamedTemporaryFile(suffix=gz_file_extension) as temp_file:
+        gzip_file(str(original_file), temp_file.name)
+        response = call_api(client, str(temp_file.name), "application/gzip", options)
+        return response
+
+
+def compare_responses(response1: httpx.Response, response2: httpx.Response, output_format: str):
+    if output_format == "application/json":
+        diff = DeepDiff(t1=response1.json(), t2=response2.json(), exclude_regex_paths=r"root\[\d+\]\['metadata'\]\['filename'\]")
+        assert len(diff) == 0
+    else:
+        df1 = pd.read_csv(io.StringIO(response1.text))
+        df2 = pd.read_csv(io.StringIO(response2.text))
+        diff = DeepDiff(t1=df1.to_dict(), t2=df2.to_dict(), exclude_regex_paths=r"root\['filename'\]\[\d+\]")
+        assert len(diff) == 0
+
+
+@pytest.mark.parametrize("output_format", ["application/json", "text/csv"])
 @pytest.mark.parametrize(
-    "example_filename, gz_uncompressed_content_type",
+    "example_filename, uncompressed_content_type",
     [
         ("fake-html.html", "text/html"),
         ("stanley-cups.csv", "application/csv"),
         ("fake.doc", "application/msword"),
         ("layout-parser-paper-fast.pdf", "application/pdf"),
+        # now the same but without explicit content type to make the system guess the un-gzipped type based on content.
+        ("fake-html.html", ""),
+        ("stanley-cups.csv", ""),
+        ("fake.doc", ""),
+        ("layout-parser-paper-fast.pdf", ""),
     ]
 )
-def test_single_gzipped_file_is_parsed_same_as_original_explicit_gz_type(example_filename: str, gz_uncompressed_content_type: str):
+def test_single_gzipped_file_is_parsed_same_as_original_explicit_gz_type(output_format: str, example_filename: str, uncompressed_content_type: str):
     """
     Verify that API supports unzipping gzip and correctly interprets gz_uncompressed_content_type
     by comparing response to directly parsing the same file.
@@ -48,13 +75,12 @@ def test_single_gzipped_file_is_parsed_same_as_original_explicit_gz_type(example
 
     original_file = Path("sample-docs") / example_filename
     client = TestClient(app)
+    gz_options = {
+        "gz_uncompressed_content_type": uncompressed_content_type if uncompressed_content_type else None,
+        "output_format": output_format
+    }
 
-    with tempfile.NamedTemporaryFile(suffix=".csv.gz") as temp_file:
-        gzip_file(str(original_file), temp_file.name)
-        response1 = call_api(client, str(temp_file.name), "application/gzip", {"gz_uncompressed_content_type": gz_uncompressed_content_type})
+    response1 = get_gzipped_response(client, str(original_file), gz_options)
+    response2 = call_api(client, str(original_file), uncompressed_content_type, {"output_format": output_format})
 
-    response2 = call_api(client, str(original_file), gz_uncompressed_content_type, {})
-
-    diff = DeepDiff(t1=response1.json(), t2=response2.json(),
-                    exclude_regex_paths=r"root\[\d+\]\['metadata'\]\['filename'\]")
-    assert len(diff) == 0
+    compare_responses(response1, response2, output_format)
